9a0b419b466a5e5aded5d17bdbe1bc440b32cd79
peterloveshim <peterloveshim@gmail.com>
2024-09-25 11:14:40 +0900

    study : pagination, placeholderData, keepPreviousData

--- pagination/src/pages/api/projects.ts
+++ pagination/src/pages/api/projects.ts
@@ -1,6 +1,11 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+  if(!req.query.page || typeof req.query.page !== 'string') {
+    throw new Error("Invalid Parameter")
+  }
+
   const page = parseInt(req.query.page) || 0
 
   const pageSize = 10
@@ -15,7 +20,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       }
     })
 
-  await new Promise((r) => setTimeout(r, 1000))
+  await new Promise((r) => setTimeout(r, 1000));
 
   res.json({ projects, hasMore: page < 9 })
 }
